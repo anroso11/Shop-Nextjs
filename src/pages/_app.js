6e09f34cbbd2bee56d636de786a87d6f5f16cc8f
anroso11 <andresroberto1187@gmail.com>
2023-02-09 11:40:39 -0500

    Inserción de Google Analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,13 +1,24 @@
-import '@styles/globals.css';
+import Script from 'next/script';
 import AppContext from '@context/AppContext';
 import useInitialState from '@hooks/useInitialState';
 import Header from '@components/Header';
+import '@styles/globals.css';
 
 
 export default function App({ Component, pageProps }) {
   const initialState = useInitialState();
   return (
     <AppContext.Provider value={initialState}>
+      <Script 
+        async src="https://www.googletagmanager.com/gtag/js?id=G-R24EBLBPB0" />
+      <Script id='google-analytics' strategy='afterInteractive'>
+        {
+          `window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-R24EBLBPB0');`
+        }
+      </Script>  
       <Header />
       <Component {...pageProps} />
     </AppContext.Provider>
